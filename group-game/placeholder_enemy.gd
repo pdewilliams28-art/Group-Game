45f56885af952cef89653fac3af49136f2f23832
biggie0cheese <nsternhagen28@bfhsla.org>
2026-03-06 15:03:39 -0600

    a
    a

--- group-game/placeholder_enemy.gd
+++ group-game/placeholder_enemy.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
-
+var target: Node2D
 
 func _physics_process(delta: float) -> void:
 	pass
-	
-	
+	chase_target(target)
 	move_and_slide()
-
+func chase_target(target):
+	pass
 
 func _on_detection_range_body_entered(body: Node2D) -> void:
 	if body is Player:
-		pass
+		target = body
