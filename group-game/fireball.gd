5e96d07087f99542e9ca8d3cd10e83b44ea7ed5a
biggie0cheese <nsternhagen28@bfhsla.org>
2026-05-14 15:05:33 -0500

    a

--- group-game/fireball.gd
+++ group-game/fireball.gd
@@ -3,7 +3,7 @@ extends Area2D
 var speed = 200
 var direction = Vector2.RIGHT
 var knockback = 100
-var damage = 100
+var damage = 50
 func _process(delta: float) -> void:
 	position += direction * speed * delta
 
