f32e0bec92c3c810c83fef1ad4342092a0842890
biggie0cheese <nsternhagen28@bfhsla.org>
2026-05-15 14:26:25 -0500

    a

---
+++ group-game/Scenes/boss_scene.gd
@@ -0,0 +1,29 @@
+extends Node2D
+@onready var pause_menu: Control = %"Pause Menu"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pause_menu.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_pause()
+	if pause_menu.paused == false:
+		toggle_pause()
+
+func toggle_pause():
+	var tree = get_tree()
+	if tree.paused == false:
+		tree.paused = true
+		pause_menu.visible = true
+		pause_menu.paused = true
+	else:
+		tree.paused = false
+		pause_menu.visible = false
+		pause_menu.paused = true
+
+
+func _on_pause_menu_resume_button_pressed() -> void:
+	toggle_pause()
